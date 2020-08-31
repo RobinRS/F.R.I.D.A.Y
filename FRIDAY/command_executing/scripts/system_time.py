b93972c0fcb3e31b900941881bb9d4b2e1c49f89
Tristi-X <49553574+Tristi-X@users.noreply.github.com>
2020-08-31 12:47:53 +0200

    Update system_time.py

--- FRIDAY/command_executing/scripts/system_time.py
+++ FRIDAY/command_executing/scripts/system_time.py
@@ -3,5 +3,5 @@ from datetime import datetime
 def cmd(entities):
     # datetime object containing current date and time
     now = datetime.now()
-    system_time = "Es ist " + str(now.strftime("%H")) + " Uhr " + str(now.strftime("%M"))
+    system_time = "It's" + str(now.strftime("%H")) + ":" + str(now.strftime("%M"))
     return system_time
