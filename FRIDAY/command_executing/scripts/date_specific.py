863a0f3ff3f2b7b8ad852bf2e676f0f9bba77508
Tristi-X <49553574+Tristi-X@users.noreply.github.com>
2020-08-31 12:52:19 +0200

    Update date_specific.py

--- FRIDAY/command_executing/scripts/date_specific.py
+++ FRIDAY/command_executing/scripts/date_specific.py
@@ -5,20 +5,20 @@ def cmd(entities):
     try:
         date_asked = entities['date_specific:date_specific'][0]['body']
     except:
-        return "Welches Datum möchtest du wissen?"
+        return "Which day do you want to know?"
 
     today = date.today()
 
-    if (date_asked == "gestern"):
+    if (date_asked == "yesterday"):
         yesterday = date.today() - timedelta(days=1)
-        dateYesterday = "Gestern war der " + str(yesterday.strftime("%d")) + ". " + str(yesterday.strftime("%m")) + ". " + str(yesterday.strftime("%Y")) + "."
+        dateYesterday = "Yesterday was the " + str(yesterday.strftime("%d")) + ". " + str(yesterday.strftime("%m")) + ". " + str(yesterday.strftime("%Y")) + "."
         return dateYesterday
 
-    elif (date_asked == "heute"):
-        dateToday = "Heute ist der " + str(today.strftime("%d")) + ". " + str(today.strftime("%m")) + ". " + str(today.strftime("%Y")) + "."
+    elif (date_asked == "today"):
+        dateToday = "Today is the " + str(today.strftime("%d")) + ". " + str(today.strftime("%m")) + ". " + str(today.strftime("%Y")) + "."
         return dateToday
 
-    elif (date_asked == "morgen"):
+    elif (date_asked == "tomorrow"):
         tomorrow = date.today() + timedelta(days=1)
-        dateTomorrow = "Morgen haben wir den " + str(tomorrow.strftime("%d")) + ". " + str(tomorrow.strftime("%m")) + ". " + str(tomorrow.strftime("%Y")) + "."
+        dateTomorrow = "Tommorrow we will have the " + str(tomorrow.strftime("%d")) + ". " + str(tomorrow.strftime("%m")) + ". " + str(tomorrow.strftime("%Y")) + "."
         return dateTomorrow
